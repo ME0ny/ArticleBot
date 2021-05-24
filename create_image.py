dd40ec10caac78aeb048acd9a35f00ec7455e695
ME0ny <ipatov.987654321@gmail.com>
2021-05-24 23:06:01 +0300

    add to class"

--- create_image.py
+++ create_image.py
@@ -1,19 +1,36 @@
 from PIL import Image, ImageDraw, ImageFont
-price = "555 "
-size = [0, 0, 36, 72, 108, 147]
-img = Image.new('RGB', (360, 446), color = (255, 230, 224))
-draw = ImageDraw.Draw(img)
-font_title = ImageFont.truetype("font/Comfortaa-Regular.ttf", 18)
-font_art = ImageFont.truetype("font/Comfortaa-Regular.ttf", 9)
-font_rub = ImageFont.truetype("font/rouble.otf", 22)
-font_price = ImageFont.truetype("font/Comfortaa-Regular.ttf", 22)
-between = 72
-n = 4
-for i in range(n):
-    draw.rounded_rectangle([(17, 193 - size[n] + 72 * i), (343,253 - size[n] + 72 * i)], radius = 8, fill = "white", outline = "white")
-    draw.text((33,206 - size[n] + 72 * i), text = "Платок", fill = "black", font = font_title)
-    draw.text((33,234 - size[n] + 72 * i), text = "арт. 4567", fill = "gray", font = font_art)
-    w, h = font_price.getsize(price)
-    draw.text((308 - w, 214 - size[n] + 72 * i), text = price, fill = "black", font = font_price)
-    draw.text((314, 214 - size[n] + 72 * i), text = "i", fill = "black", font = font_rub)
-img.save('pil_text.png')
+
+class Generate:
+    def __init__(self, background = (255, 230, 224), size = (360, 446)):
+        self.font_title = ImageFont.truetype("font/Comfortaa-Regular.ttf", 18)
+        self.font_art = ImageFont.truetype("font/Comfortaa-Regular.ttf", 9)
+        self.font_rub = ImageFont.truetype("font/rouble.otf", 22)
+        self.font_price = ImageFont.truetype("font/Comfortaa-Regular.ttf", 22)
+
+        self.size = [0, 0, 36, 72, 108, 147]
+        self.between_block = 72
+
+        self.img = Image.new('RGB', size, color = background)
+        self.draw = ImageDraw.Draw(self.img)
+
+    def draw_content(self, quantity, price, artical, title):
+        price = [str(i) for i in price]
+        artical = ["арт. "+ str (i) for i in artical]
+        for i in range(quantity):
+            self.draw.rounded_rectangle([(17, 193 - self.size[quantity] + self.between_block * i),
+                (343,253 - self.size[quantity] + self.between_block * i)],
+                radius = 8, fill = "white", outline = "white")
+            self.draw.text((33,206 - self.size[quantity] + self.between_block * i), text = title[i], fill = "black", font = self.font_title)
+            self.draw.text((33,234 - self.size[quantity] + self.between_block * i), text = artical[i], fill = "gray", font = self.font_art)
+            w, h = self.font_price.getsize(price[i])
+            self.draw.text((308 - w, 214 - self.size[quantity] + self.between_block * i), text = price[i], fill = "black", font = self.font_price)
+            self.draw.text((314, 214 - self.size[quantity] + self.between_block * i), text = "i", fill = "black", font = self.font_rub)
+    
+    def show(self):
+        print(type(self.img))
+        self.img.save('pil_text.png')
+
+img = Generate()
+img.draw_content(4, [711, 6500, 2599, 2990], [11, 123, 15, 164],
+["Платок", "Пиджка", "Футболка", "Джинсы"])
+img.show()
