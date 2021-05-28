a2be44494469285eae46212aa60e0b76dee63d01
ME0ny <ipatov.987654321@gmail.com>
2021-05-28 15:20:48 +0300

    fix resolution, up to hd

--- create_image.py
+++ create_image.py
@@ -1,14 +1,17 @@
 from PIL import Image, ImageDraw, ImageFont
 
 class Generate:
-    def __init__(self, background = (255, 230, 224), size = (360, 446)):
-        self.font_title = ImageFont.truetype("font/Comfortaa-Regular.ttf", 18)
-        self.font_art = ImageFont.truetype("font/Comfortaa-Regular.ttf", 9)
-        self.font_rub = ImageFont.truetype("/home/PriceBot/bot/font/rouble.ttf", 22)
-        self.font_price = ImageFont.truetype("font/Comfortaa-Regular.ttf", 22)
-
-        self.size = [0, 0, 36, 72, 108, 147]
-        self.between_block = 72
+    def __init__(self, background = (242, 219, 211), size = (1024, 1280)):
+        title_font_size = 60
+        art_font_size = 25
+        rub_font_size = 62
+        price_font_size = 62
+        self.font_title = ImageFont.truetype("font/Comfortaa-VariableFont_wght.ttf", title_font_size)
+        self.font_art = ImageFont.truetype("font/Comfortaa-Regular.ttf", art_font_size)
+        self.font_rub = ImageFont.truetype("font/rouble.ttf", rub_font_size)
+        self.font_price = ImageFont.truetype("font/Comfortaa-Regular.ttf", price_font_size)
+        self.size = [0, 0, 103, 206, 309, 412]
+        self.between_block = 206
 
         self.img = Image.new('RGB', size, color = background)
         self.draw = ImageDraw.Draw(self.img)
@@ -17,14 +20,14 @@ class Generate:
         price = [str(i) for i in price]
         artical = ["арт. " + str (i) for i in artical]
         for i in range(quantity):
-            self.draw.rounded_rectangle([(17, 193 - self.size[quantity] + self.between_block * i),
-                (343,253 - self.size[quantity] + self.between_block * i)],
-                radius = 8, fill = "white", outline = "white")
-            self.draw.text((33,206 - self.size[quantity] + self.between_block * i), text = title[i], fill = "black", font = self.font_title)
-            self.draw.text((33,234 - self.size[quantity] + self.between_block * i), text = artical[i], fill = "gray", font = self.font_art)
+            self.draw.rounded_rectangle([(49, 554 - self.size[quantity] + self.between_block * i),
+                (976, 726 - self.size[quantity] + self.between_block * i)],
+                radius = 22, fill = "white", outline = "white")
+            self.draw.text((91,588 - self.size[quantity] + self.between_block * i), text = title[i], fill = "black", font = self.font_title)
+            self.draw.text((91,670 - self.size[quantity] + self.between_block * i), text = artical[i], fill = "gray", font = self.font_art)
             w, h = self.font_price.getsize(price[i])
-            self.draw.text((308 - w, 214 - self.size[quantity] + self.between_block * i), text = price[i], fill = "black", font = self.font_price)
-            self.draw.text((314, 214 - self.size[quantity] + self.between_block * i), text = "i", fill = "black", font = self.font_rub)
+            self.draw.text((866 - w, 608 - self.size[quantity] + self.between_block * i), text = price[i], fill = "black", font = self.font_price)
+            self.draw.text((894, 612 - self.size[quantity] + self.between_block * i), text = "i", fill = "black", font = self.font_rub)
 
     def show(self):
         self.img.save('1' + '.png')
